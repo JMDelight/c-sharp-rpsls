3cd000e18a889fe54e783c78310dc599eb096622
Carl Egbert and Joel Delight <carl+joel@>
2016-07-06 14:33:29 -0700

    update styling. add scoreboard feature.

--- Objects/rslpsGame.cs
+++ Objects/rslpsGame.cs
@@ -8,13 +8,14 @@ namespace rslps.Objects
     private int _choiceOne;
     private int _choiceTwo;
     private int _difference;
+    private static int _playerOneScore = 0;
+    private static int _playerTwoScore = 0;
 
     public rslpsGame(int choiceOne, int choiceTwo = -1)
     {
       _choiceOne = choiceOne;
       if (choiceTwo==-1) _choiceTwo = rslpsRandom();
       else _choiceTwo = choiceTwo;
-      Console.WriteLine("1:"+_choiceOne+"2:"+_choiceTwo);
       // -- adding 5 to the difference to allow % to work with negative numbers as if they were an extension of positive numbers -- //
       _difference = 5 + _choiceOne - _choiceTwo;
     }
@@ -29,12 +30,28 @@ namespace rslps.Objects
       return gameOptions[_choiceTwo];
     }
 
+    public int GetPlayerOneScore()
+    {
+      return _playerOneScore;
+    }
+
+    public int GetPlayerTwoScore()
+    {
+      return _playerTwoScore;
+    }
+
     public string rslpsFight()
     {
       string result;
 
-      if (_difference % 5 == 4 || _difference % 5 == 3) result = "Player One Wins";
-      else if (_difference % 5 == 1 || _difference % 5 == 2) result = "Player Two Wins";
+      if (_difference % 5 == 4 || _difference % 5 == 3) {
+        result = "Player One Wins";
+        _playerOneScore++;
+      }
+      else if (_difference % 5 == 1 || _difference % 5 == 2){
+        result = "Player Two Wins";
+        _playerTwoScore++;
+      }
       else if (_choiceOne == _choiceTwo) result = "Tie";
       else result = "Error!";
       return result;
@@ -44,6 +61,13 @@ namespace rslps.Objects
     {
       Random random = new Random();
       return random.Next(0,5);
+    }
+    public string rslpsImageNamer(string choiceName)
+    {
+      string returnValue = "Content/img/";
+      returnValue += choiceName;
+      returnValue += ".png";
+      return returnValue;
     }
   }
 }
