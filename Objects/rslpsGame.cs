4d7a80df64dec3ea8b14c5c710a0a411b4d94757
Carl Egbert and Joel Delight <carl+joel@>
2016-07-06 13:17:43 -0700

    refactor win checking method. replace overloaded constructor with single constructor with optional argument

--- Objects/rslpsGame.cs
+++ Objects/rslpsGame.cs
@@ -7,17 +7,16 @@ namespace rslps.Objects
     private static string [] gameOptions = new string [5] {"Rock", "Scissors", "Lizard", "Paper", "Spock"};
     private int _choiceOne;
     private int _choiceTwo;
+    private int _difference;
 
-    public rslpsGame(int choiceOne)
+    public rslpsGame(int choiceOne, int choiceTwo = -1)
     {
       _choiceOne = choiceOne;
-      _choiceTwo = rslpsRandom();
-    }
-
-    public rslpsGame(int choiceOne, int choiceTwo)
-    {
-      _choiceOne = choiceOne;
-      _choiceTwo = choiceTwo;
+      if (choiceTwo==-1) _choiceTwo = rslpsRandom();
+      else _choiceTwo = choiceTwo;
+      Console.WriteLine("1:"+_choiceOne+"2:"+_choiceTwo);
+      // -- adding 5 to the difference to allow % to work with negative numbers as if they were an extension of positive numbers -- //
+      _difference = 5 + _choiceOne - _choiceTwo;
     }
 
     public string GetChoiceOne()
@@ -33,8 +32,9 @@ namespace rslps.Objects
     public string rslpsFight()
     {
       string result;
-      if (_choiceOne-_choiceTwo == -1 || _choiceOne-_choiceTwo == -2 || _choiceOne-_choiceTwo == 4 || _choiceOne-_choiceTwo == 3) result = "Player One Wins";
-      else if (_choiceOne-_choiceTwo == 1 || _choiceOne-_choiceTwo == 2 || _choiceOne-_choiceTwo == -4 || _choiceOne-_choiceTwo == -3) result = "Player Two Wins";
+
+      if (_difference % 5 == 4 || _difference % 5 == 3) result = "Player One Wins";
+      else if (_difference % 5 == 1 || _difference % 5 == 2) result = "Player Two Wins";
       else if (_choiceOne == _choiceTwo) result = "Tie";
       else result = "Error!";
       return result;
